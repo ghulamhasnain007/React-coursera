93235ea6765903f4f4a46bad59fc4a93bb1ce7e6
ghulamhasnain007 <ghasnain668@gmail.com>
2024-02-27 11:15:49 +0500

    replacement

--- Files/src/components/LandingSection.js
+++ Files/src/components/LandingSection.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
+import Front from '../images/unnamed.jpg'
 
-const greeting = "Hello, I am Pete!";
+const greeting = "Hello, I am Ghulam Hasnain!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
@@ -15,6 +16,19 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
+    <VStack spacing={16}>
+      <Avatar src={Front}
+      size='2x1'
+      name="Ghulam Hasnain"
+      />
+      <Heading as='h4' size="md" noOfLines={1}>{greeting}</Heading>
+
+      <VStack spacing={6}>
+        <Heading as='h1' size='lg' noOfLines={1}>{bio1}</Heading>
+        <Heading as='h1' size='lg' noOfLines={1}>{bio2}</Heading>
+
+      </VStack>
+      </VStack>
 
   </FullScreenSection>
 );
